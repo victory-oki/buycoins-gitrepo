d983cdc48f082e0df4c6832759160c37de295c31
victory-oki <victorysplogi@gmail.com>
2020-11-27 21:28:05 +0100

    add env variable

--- index.js
+++ index.js
@@ -196,7 +196,7 @@ var controller = (function(model, view){
     var fetchRepositoryData = function(){
         fetch('https://api.github.com/graphql',{
             method:'POST',
-            headers: { "Content-Type": "application/json", "Authorization": "Bearer 67e1358e29e11ac118bf419f8e9b8d9cb852490a"},
+            headers: { "Content-Type": "application/json", "Authorization": `Bearer ${process.env.ACCESS_TOKEN}` },
             body: JSON.stringify({
                 query: model.fetchRepoQuery
             })
